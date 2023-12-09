c71457551a3c1a98766b69ad478984733c54c739
Sayantan Paul <sayantanpaul222@gmail.com>
2023-12-09 12:31:48 +0530

    form done

--- submit.php
+++ submit.php
@@ -1,4 +1,15 @@
 <?php
-phpinfo();
-echo 'hello'
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "form_data";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set PDO to throw exceptions on errors
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
