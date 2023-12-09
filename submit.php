bb47de084e0b5ad0ba6a8903921afc29404bc1eb
Sayantan Paul <sayantanpaul222@gmail.com>
2023-12-09 12:33:47 +0530

    form done

--- submit.php
+++ submit.php
@@ -1,15 +1,36 @@
 <?php
+// Database configuration
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "form_data";
 
 try {
+    // Establishing a connection to the database using PDO
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Set PDO to throw exceptions on errors
+    
+    // Set PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    // Prepare SQL statement to insert data into table
+    $stmt = $conn->prepare("INSERT INTO form_data (name, email, phone) VALUES (:name, :email, :phone)");
+
+    // Bind parameters and execute the statement
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':phone', $phone);
+
+    $stmt->execute();
+
+    echo "New record created successfully";
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Error: " . $e->getMessage();
 }
+
+$conn = null; // Close connection
 ?>
